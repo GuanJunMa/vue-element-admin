9e72efc4d41af8caefc45a09867d5983aea17767
GuanJunMa <858324356@qq.com>
2019-02-19 23:53:59 +0800

    feat: test cz

---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg
+    :class="svgClass"
+    aria-hidden="true"
+  >
+    <use :xlink:href="iconName" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'SvgIcon',
+  props: {
+    iconClass: {
+      type: String,
+      required: true
+    },
+    className: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
+
